9ec740813764cb08480b51a6d917b169023a923b
Thomas Farineau <thomas.farineau@icloud.com>
2022-10-03 10:59:56 +0200

    add client side connection choice

--- client/src/MainClient.java
+++ client/src/MainClient.java
@@ -1,6 +1,49 @@
+import exceptions.InvalidCredentialsException;
+import exceptions.SignUpFailed;
+import interfaces.IConnection;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
 public class MainClient {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 2001);
+            IConnection connection = (IConnection) registry.lookup("Connection");
+            chooseAction(connection);
+        } catch (Exception e) {
+            System.out.println("Client err: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void chooseAction(IConnection connection) throws RemoteException, SignUpFailed, InvalidCredentialsException {
+        while(true) {
+            System.out.println("1. Sign up");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Mail: ");
+                    String mail = scanner.next();
+                    System.out.println("Password: ");
+                    String pwd = scanner.next();
+                    connection.signUp(mail, pwd);
+                }
+                case 2 -> {
+                    System.out.println("Mail: ");
+                    String mail = scanner.next();
+                    System.out.println("Password: ");
+                    String pwd = scanner.next();
+                    connection.login(mail, pwd);
+                }
+                case 3 -> System.exit(0);
+            }
+        }
     }
 
 }
--- server/src/implem/MainServer.java
+++ server/src/implem/MainServer.java
@@ -9,9 +9,9 @@ import java.rmi.registry.Registry;
 public class MainServer {
     public static void main(String[] args) {
         try {
-            Registry reg = LocateRegistry.createRegistry(2001);
-            IConnection d = new Connection();
-            reg.rebind("MonOD", d);
+            Registry registry = LocateRegistry.createRegistry(2001);
+            IConnection connection = new Connection();
+            registry.rebind("Connection", connection);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
